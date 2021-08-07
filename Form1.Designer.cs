0f20ec9cf767f6dcc9e284fa2c7810afa84f0d73
QUISPE SANTOS JEREMY HAROL <CI705428402020@jpardo.edu.pe>
2021-08-07 12:44:05 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace ejercicion20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblDocientos = new System.Windows.Forms.Label();
+            this.lblCien = new System.Windows.Forms.Label();
+            this.lblCincuenta = new System.Windows.Forms.Label();
+            this.lblVeinte = new System.Windows.Forms.Label();
+            this.lblDiez = new System.Windows.Forms.Label();
+            this.lblCinco = new System.Windows.Forms.Label();
+            this.lblDos = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtDocientos = new System.Windows.Forms.TextBox();
+            this.txtCien = new System.Windows.Forms.TextBox();
+            this.txtCincuenta = new System.Windows.Forms.TextBox();
+            this.txtVeinte = new System.Windows.Forms.TextBox();
+            this.txtDiez = new System.Windows.Forms.TextBox();
+            this.txtCinco = new System.Windows.Forms.TextBox();
+            this.txtDos = new System.Windows.Forms.TextBox();
+            this.txtUno = new System.Windows.Forms.TextBox();
+            this.lblUno = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(30, 24);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(77, 17);
+            this.lblCantidad.TabIndex = 0;
+            this.lblCantidad.Text = "CANTIDAD";
+            // 
+            // lblDocientos
+            // 
+            this.lblDocientos.AutoSize = true;
+            this.lblDocientos.Location = new System.Drawing.Point(51, 61);
+            this.lblDocientos.Name = "lblDocientos";
+            this.lblDocientos.Size = new System.Drawing.Size(44, 17);
+            this.lblDocientos.TabIndex = 1;
+            this.lblDocientos.Text = "$.200";
+            // 
+            // lblCien
+            // 
+            this.lblCien.AutoSize = true;
+            this.lblCien.Location = new System.Drawing.Point(51, 85);
+            this.lblCien.Name = "lblCien";
+            this.lblCien.Size = new System.Drawing.Size(44, 17);
+            this.lblCien.TabIndex = 2;
+            this.lblCien.Text = "$.100";
+            // 
+            // lblCincuenta
+            // 
+            this.lblCincuenta.AutoSize = true;
+            this.lblCincuenta.Location = new System.Drawing.Point(51, 117);
+            this.lblCincuenta.Name = "lblCincuenta";
+            this.lblCincuenta.Size = new System.Drawing.Size(36, 17);
+            this.lblCincuenta.TabIndex = 3;
+            this.lblCincuenta.Text = "$.50";
+            // 
+            // lblVeinte
+            // 
+            this.lblVeinte.AutoSize = true;
+            this.lblVeinte.Location = new System.Drawing.Point(51, 145);
+            this.lblVeinte.Name = "lblVeinte";
+            this.lblVeinte.Size = new System.Drawing.Size(36, 17);
+            this.lblVeinte.TabIndex = 4;
+            this.lblVeinte.Text = "$.20";
+            // 
+            // lblDiez
+            // 
+            this.lblDiez.AutoSize = true;
+            this.lblDiez.Location = new System.Drawing.Point(51, 192);
+            this.lblDiez.Name = "lblDiez";
+            this.lblDiez.Size = new System.Drawing.Size(36, 17);
+            this.lblDiez.TabIndex = 5;
+            this.lblDiez.Text = "$.10";
+            // 
+            // lblCinco
+            // 
+            this.lblCinco.AutoSize = true;
+            this.lblCinco.Location = new System.Drawing.Point(51, 231);
+            this.lblCinco.Name = "lblCinco";
+            this.lblCinco.Size = new System.Drawing.Size(28, 17);
+            this.lblCinco.TabIndex = 6;
+            this.lblCinco.Text = "$.5";
+            // 
+            // lblDos
+            // 
+            this.lblDos.AutoSize = true;
+            this.lblDos.Location = new System.Drawing.Point(51, 268);
+            this.lblDos.Name = "lblDos";
+            this.lblDos.Size = new System.Drawing.Size(28, 17);
+            this.lblDos.TabIndex = 7;
+            this.lblDos.Text = "$.2";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(113, 24);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 22);
+            this.txtCantidad.TabIndex = 8;
+            // 
+            // txtDocientos
+            // 
+            this.txtDocientos.Location = new System.Drawing.Point(113, 61);
+            this.txtDocientos.Name = "txtDocientos";
+            this.txtDocientos.Size = new System.Drawing.Size(100, 22);
+            this.txtDocientos.TabIndex = 9;
+            // 
+            // txtCien
+            // 
+            this.txtCien.Location = new System.Drawing.Point(113, 89);
+            this.txtCien.Name = "txtCien";
+            this.txtCien.Size = new System.Drawing.Size(100, 22);
+            this.txtCien.TabIndex = 10;
+            // 
+            // txtCincuenta
+            // 
+            this.txtCincuenta.Location = new System.Drawing.Point(113, 117);
+            this.txtCincuenta.Name = "txtCincuenta";
+            this.txtCincuenta.Size = new System.Drawing.Size(100, 22);
+            this.txtCincuenta.TabIndex = 11;
+            // 
+            // txtVeinte
+            // 
+            this.txtVeinte.Location = new System.Drawing.Point(113, 145);
+            this.txtVeinte.Name = "txtVeinte";
+            this.txtVeinte.Size = new System.Drawing.Size(100, 22);
+            this.txtVeinte.TabIndex = 12;
+            // 
+            // txtDiez
+            // 
+            this.txtDiez.Location = new System.Drawing.Point(113, 187);
+            this.txtDiez.Name = "txtDiez";
+            this.txtDiez.Size = new System.Drawing.Size(100, 22);
+            this.txtDiez.TabIndex = 13;
+            // 
+            // txtCinco
+            // 
+            this.txtCinco.Location = new System.Drawing.Point(113, 231);
+            this.txtCinco.Name = "txtCinco";
+            this.txtCinco.Size = new System.Drawing.Size(100, 22);
+            this.txtCinco.TabIndex = 14;
+            // 
+            // txtDos
+            // 
+            this.txtDos.Location = new System.Drawing.Point(113, 268);
+            this.txtDos.Name = "txtDos";
+            this.txtDos.Size = new System.Drawing.Size(100, 22);
+            this.txtDos.TabIndex = 15;
+            // 
+            // txtUno
+            // 
+            this.txtUno.Location = new System.Drawing.Point(113, 296);
+            this.txtUno.Name = "txtUno";
+            this.txtUno.Size = new System.Drawing.Size(100, 22);
+            this.txtUno.TabIndex = 17;
+            // 
+            // lblUno
+            // 
+            this.lblUno.AutoSize = true;
+            this.lblUno.Location = new System.Drawing.Point(51, 299);
+            this.lblUno.Name = "lblUno";
+            this.lblUno.Size = new System.Drawing.Size(28, 17);
+            this.lblUno.TabIndex = 16;
+            this.lblUno.Text = "$.1";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(263, 49);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(104, 41);
+            this.btnCalcular.TabIndex = 18;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtUno);
+            this.Controls.Add(this.lblUno);
+            this.Controls.Add(this.txtDos);
+            this.Controls.Add(this.txtCinco);
+            this.Controls.Add(this.txtDiez);
+            this.Controls.Add(this.txtVeinte);
+            this.Controls.Add(this.txtCincuenta);
+            this.Controls.Add(this.txtCien);
+            this.Controls.Add(this.txtDocientos);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblDos);
+            this.Controls.Add(this.lblCinco);
+            this.Controls.Add(this.lblDiez);
+            this.Controls.Add(this.lblVeinte);
+            this.Controls.Add(this.lblCincuenta);
+            this.Controls.Add(this.lblCien);
+            this.Controls.Add(this.lblDocientos);
+            this.Controls.Add(this.lblCantidad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblDocientos;
+        private System.Windows.Forms.Label lblCien;
+        private System.Windows.Forms.Label lblCincuenta;
+        private System.Windows.Forms.Label lblVeinte;
+        private System.Windows.Forms.Label lblDiez;
+        private System.Windows.Forms.Label lblCinco;
+        private System.Windows.Forms.Label lblDos;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtDocientos;
+        private System.Windows.Forms.TextBox txtCien;
+        private System.Windows.Forms.TextBox txtCincuenta;
+        private System.Windows.Forms.TextBox txtVeinte;
+        private System.Windows.Forms.TextBox txtDiez;
+        private System.Windows.Forms.TextBox txtCinco;
+        private System.Windows.Forms.TextBox txtDos;
+        private System.Windows.Forms.TextBox txtUno;
+        private System.Windows.Forms.Label lblUno;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicion20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int cantidad = int.Parse(txtCantidad.Text);
+
+            int cdocientos = cantidad / 200;
+            cantidad %= 200;
+            int ccien = cantidad / 100;
+            cantidad %= 100;
+            int ccincuenta = cantidad / 50;
+            cantidad %= 50;
+            int cveinte = cantidad / 20;
+            cantidad %= 20;
+            int cdiez = cantidad / 10;
+            cantidad %= 10;
+            int ccinco = cantidad / 5;
+            cantidad %= 5;
+            int cdos = cantidad / 2;
+            int cuno = cantidad / 1;
+
+            txtDocientos.Text = "" + cdocientos;
+            txtCien.Text = "" + ccien;
+            txtCincuenta = "" + ccincuenta;
+            txtVeinte = "" + cveinte;
+            txtDiez.Text = "" + cdiez;
+            txtDos.Text = "" + cdos;
+            txtUno.Text = "" + cuno;
+            
+            
+
+
+        }
+    }
+}
